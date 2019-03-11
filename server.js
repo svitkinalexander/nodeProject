6c01d83c1cf040bbff0d2fc3d2ad388567b321cc
Egor <ieghor-popov-2000@mail.ru>
2019-03-11 21:37:49 +0300

    fix: remov bug with ports

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.get("/", function (request, response) {
     response.send("Главная страница");
 });
 
-app.listen(8080);
+app.listen(process.env.PORT || 8080);
 console.log('Server running on port 8080.');
 
 
