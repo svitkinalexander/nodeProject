357851fc0379d75ee6aac25576b53c90f1324ecf
Egor <ieghor-popov-2000@mail.ru>
2019-03-16 22:28:17 +0300

    feat: add insert into table

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const mysql = require('mysql');
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 let app = express();
 const connection = mysql.createConnection({
-    host: "localhost",//us-cdbr-iron-east-03.cleardb.net
-    user: "root",//b19cd822bf8582
-    password: "password",//bc3ac634
-    database: 'remontdb'//heroku_0797947479f038e
+    host: "us-cdbr-iron-east-03.cleardb.net",//us-cdbr-iron-east-03.cleardb.net
+    user: "b19cd822bf8582",//b19cd822bf8582
+    password: "bc3ac634",//bc3ac634
+    database: 'heroku_0797947479f038e'//heroku_0797947479f038e
 });
 
 app.use(express.static(__dirname + '/public'));
