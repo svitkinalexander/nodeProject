532c91a217165de7436583eab773ec49841a9b11
Egor <ieghor-popov-2000@mail.ru>
2019-03-16 22:26:00 +0300

    feat: add insert into table

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const mysql = require('mysql');
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 let app = express();
+const connection = mysql.createConnection({
+    host: "localhost",//us-cdbr-iron-east-03.cleardb.net
+    user: "root",//b19cd822bf8582
+    password: "password",//bc3ac634
+    database: 'remontdb'//heroku_0797947479f038e
+});
+
 app.use(express.static(__dirname + '/public'));
 app.get("/imei", urlencodedParser, function (request, response) {
     response.sendFile(__dirname + "/imei.html");
@@ -13,14 +20,8 @@ app.get("/imei", urlencodedParser, function (request, response) {
 app.post("/imei", urlencodedParser, function (request, response) {
     if (!request.body) return response.sendStatus(400);
     console.log(request.body);
-    let connection = mysql.createConnection({
-        host: "us-cdbr-iron-east-03.cleardb.net",
-        user: "us",
-        password: "pas",
-        database: 'heroku_0797947479f038e'
-    });
 
-     connection.query(`SELECT * FROM Remont WHERE imei = ${request.body.words};`, function (error, result, fields) {
+     connection.query(`SELECT * FROM Remont WHERE imei = ${request.body.words};`, function (error, result) {
          if (error) {
              return response.status(400).json({ error: error.message });
         };
@@ -34,16 +35,28 @@ app.post("/imei", urlencodedParser, function (request, response) {
 app.get("/register", urlencodedParser, function (request, response) {
     response.sendFile(__dirname + "/register.html");
 });
+
 app.post("/register", urlencodedParser, function (request, response) {
+    connection.query(`INSERT INTO Remont (imei, model, status, cost) VALUES ('${request.body.imei}', '${request.body.device}', 'Not OK', '20 BYN')`, function (error) {
+        if (error) {
+            return response.status(400).json({ error: error.message });
+        };
+        console.log('Table successfully updated!');
+        //connection.end(); 
+    });
+/* });
+
+app.post("/register", urlencodedParser, function (request, response) { */
     if (!request.body) return response.sendStatus(400);
     console.log(request.body);
+
     const transporter = nodemailer.createTransport({
         host: 'smtp.mail.ru',
         port: 465,
         secure: true, //true --> will use ssl
         auth: {
-            user: 'us',
-            pass: 'pas'
+            user: 'ieghor-popov-2000@mail.ru',
+            pass: 'Gb7k854A'
         }
     });
 
@@ -54,6 +67,8 @@ app.post("/register", urlencodedParser, function (request, response) {
         text: 'Поломка',
         html: `<b>Name:<b> ${request.body.name};<br>
                <b>Email:<b> ${request.body.email};<br>
+               <b>Imei:<b> ${request.body.imei};<br>
+               <b>Устройство:<b> ${request.body.device};<br>
                <b>Поломка:<b> ${request.body.message};<br>`
     };
 
@@ -65,6 +80,7 @@ app.post("/register", urlencodedParser, function (request, response) {
         }
         transporter.close();
     });
+
     /* response.send(); */
     response.writeHead(302, {
         'Location': 'index.html'
