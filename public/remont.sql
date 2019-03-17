eaba0572e1ba333d547bc75738f679adff4d7454
Nemkev <ostegoelectric@gmail.com>
2019-03-17 14:00:46 +0300

    docs: add more content

--- public/remont.sql
+++ public/remont.sql
@@ -1,5 +1,5 @@
 
-USE remontdb;
+USE heroku_0797947479f038e;
 SET NAMES 'cp1251';
 SET CHARACTER SET 'cp1251';
 CREATE TABLE Remont
@@ -12,9 +12,24 @@ CREATE TABLE Remont
 DEFAULT CHARACTER SET = 'utf8';
 
 INSERT INTO Remont (`imei`, `model`, `status`, `cost`) VALUES
-(1, 'Sony', 'OK', '200 RUB'),
-(6, 'Sony', 'OK', '200 RUB.'),
+(1, 'Sony phone', 'OK', '200 BYN'),
+(6, 'Sony', 'OK', '200 BYN.'),
 (5, 'Iphone 5s', 'OK', '40 GRI'),
-(2, 'Samsung', 'Not OK', '25 RUB'),
-(3, 'Nokia', 'OK', '36 RUB'),
-(4, 'Xiaomi', 'OK', '24 RUB');
+(2, 'Samsung g2', 'Not OK', '25 BYN'),
+(3, 'Nokia', 'OK', '36 BYN'),
+(4, 'Iphone X', 'Not OK', '46 BYN'),
+(7, 'Xiaomi Redmi note', 'OK', '23 BYN'),
+(9, 'Samsung Galaxy s5', 'OK', '142 BYN'),
+(8, 'Xperia a4', 'OK', '56 USD'),
+(11, 'Blackberry', 'Not OK', '115 USD'),
+(10, 'PS 4', 'OK', '58 BYN'),
+(12, 'Xbox One X', 'OK', '93 BYN'),
+(13, 'Macbook pro', 'OK', '543 BYN'),
+(18, 'Mystery monitor', 'OK', '25 EU'),
+(16, 'Iphone 8', 'OK', '98 USD'),
+(14, 'Dell Alienware 17 R4', 'Not OK', '326 BYN'),
+(15, 'Asus ROG 2', 'OK', '73 BYN'),
+(56, 'LG Monitor r5', 'Not OK', '76 BYN'),
+(24, 'Lenovo lEGION Y530', 'OK', '210 BYN'),
+(48, 'Acer Predator', 'OK', '33 BYN'),
+(19, 'Xiaomi', 'OK', '24 BYN');
--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const mysql = require('mysql');
-const fs = require('fs');
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 let app = express();
+const connection = mysql.createConnection({
+    host: "localhost",//us-cdbr-iron-east-03.cleardb.net
+    user: "root",//b19cd822bf8582
+    password: "password",//bc3ac634
+    database: 'remontdb'//heroku_0797947479f038e
+});
+
 app.use(express.static(__dirname + '/public'));
 app.get("/imei", urlencodedParser, function (request, response) {
     response.sendFile(__dirname + "/imei.html");
@@ -14,19 +20,13 @@ app.get("/imei", urlencodedParser, function (request, response) {
 app.post("/imei", urlencodedParser, function (request, response) {
     if (!request.body) return response.sendStatus(400);
     console.log(request.body);
-    let connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "password",
-        database: 'remontdb'
-    });
 
-     connection.query('SELECT * FROM Remont;', function (error, result, fields) {
+     connection.query(`SELECT * FROM Remont WHERE imei = ${request.body.words};`, function (error, result) {
          if (error) {
              return response.status(400).json({ error: error.message });
         };
         console.log('result: ', result);
-          response.render('data.hbs', { data: JSON.stringify(result)});
+          response.render('data.hbs', { data: result[0]});
          //response.end(JSON.stringify(result)); 
          //return response.render('data.hbs', { data: JSON.stringify(result)});//{{data.result}}
     });
@@ -35,16 +35,28 @@ app.post("/imei", urlencodedParser, function (request, response) {
 app.get("/register", urlencodedParser, function (request, response) {
     response.sendFile(__dirname + "/register.html");
 });
+
 app.post("/register", urlencodedParser, function (request, response) {
+    connection.query(`INSERT INTO Remont (imei, model, status, cost) VALUES ('${request.body.imei}', '${request.body.device}', 'Not OK', '20 BYN')`, function (error) {
+        if (error) {
+            return response.status(400).json({ error: error.message });
+        };
+        console.log('Table successfully updated!');
+        //connection.end(); 
+    });
+/* });
+
+app.post("/register", urlencodedParser, function (request, response) { */
     if (!request.body) return response.sendStatus(400);
     console.log(request.body);
+
     const transporter = nodemailer.createTransport({
         host: 'smtp.mail.ru',
         port: 465,
         secure: true, //true --> will use ssl
         auth: {
-            user: 'youmail',
-            pass: 'youpassword'
+            user: 'ieghor-popov-2000@mail.ru',
+            pass: 'Gb7k854A'
         }
     });
 
@@ -55,6 +67,8 @@ app.post("/register", urlencodedParser, function (request, response) {
         text: 'Поломка',
         html: `<b>Name:<b> ${request.body.name};<br>
                <b>Email:<b> ${request.body.email};<br>
+               <b>Imei:<b> ${request.body.imei};<br>
+               <b>Устройство:<b> ${request.body.device};<br>
                <b>Поломка:<b> ${request.body.message};<br>`
     };
 
@@ -66,6 +80,7 @@ app.post("/register", urlencodedParser, function (request, response) {
         }
         transporter.close();
     });
+
     /* response.send(); */
     response.writeHead(302, {
         'Location': 'index.html'
@@ -73,9 +88,9 @@ app.post("/register", urlencodedParser, function (request, response) {
     response.end();
 });
 
-app.get("/", function (request, response) {
+/* app.get("/", function (request, response) {
     response.send("Главная страница");
-});
+}); */
 
 app.listen(process.env.PORT || 8080);
 console.log('Server running on port 8080.');
